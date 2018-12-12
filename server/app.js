8afd6de3149ac8af5aee997807656677f415ebbe
pragistyo <new.SOS.hope@outlook.com>
2018-12-12 14:51:39 +0700

    remove mongo cred
    remove mongo cred

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 // mongoose.connect(`mongodb://localhost/hactiv-overflow`,(err)=>{
-mongoose.connect(`mongodb://ogi-overflow:ogitampan@cluster0-shard-00-00-nsxhr.mongodb.net:27017,cluster0-shard-00-01-nsxhr.mongodb.net:27017,cluster0-shard-00-02-nsxhr.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`, (err) => {
+mongoose.connect(`mongodb://{mongo_id}:{mongo_password}@cluster0-shard-00-00-nsxhr.mongodb.net:27017,cluster0-shard-00-01-nsxhr.mongodb.net:27017,cluster0-shard-00-02-nsxhr.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`, (err) => {
   if(!err){
     console.log('MONGOOSE connected');
   }else{
